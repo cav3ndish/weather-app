59e9987c8441f709d5b7822a220867588e1ee5a5
cav3ndish <73221879+cav3ndish@users.noreply.github.com>
2021-01-18 16:44:28 +0100

    imported bootstrap and axios and built the weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
+export default function App() {
+  let [city, setCity] = useState(null);
+  let [lineOne, setLineOne] = useState(null);
+
+  function showWeather(response) {
+    let temperature = Math.round(response.data.main.temp);
+    let wind = Math.round(response.data.wind.speed);
+    let humidity = response.data.main.humidity;
+    let description = response.data.weather[0].description;
+    setLineOne(
+      <div>
+        <ul className="List">
+          <li>City: {city} </li>
+          <li>
+            {" "}
+            <img
+              src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+              alt="icon"
+            ></img>
+          </li>
+          <li> Temperature: {temperature}°C </li>
+          <li> Looks like: {description}</li>
+          <li> Wind speed: {wind} km/h </li>
+          <li>Humidity: {humidity}%</li>
+        </ul>
+      </div>
+    );
+  }
+  function showCity(event) {
+    setCity(
+      event.target.value.charAt(0).toUpperCase() + event.target.value.slice(1)
+    );
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0a0b749fb3632bec51c7fbeb7af687a1&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className="search-form Search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="form-control"
+          autoFocus={true}
+          required={true}
+          onChange={showCity}
+          placeholder="Enter a city"
+        />
+        <input type="submit" className="btn btn-info" value="Submit" />
+      </form>
+
+      {lineOne}
     </div>
   );
 }
 
-export default App;
